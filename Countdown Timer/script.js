fec25458e63cdb6ca28877c0bd5eb41d51b9b6a0
Abhishek Gurjar <120670964+abhishekgurjar-in@users.noreply.github.com>
2024-09-03 13:02:16 +0530

    Add files via upload

---
+++ Countdown Timer/script.js
@@ -0,0 +1,20 @@
+const endDate = "4 July 2025 10:00 PM";
+
+document.getElementById("end-date").innerText = endDate;
+const input = document.querySelectorAll("input");
+
+function countDown() {
+  const end = new Date(endDate);
+  const now = new Date();
+  const diff = (end - now) / 1000;
+
+  if (diff < 0) return;
+
+  input[0].value = Math.floor(diff / 3600 / 24);
+  input[1].value = Math.floor(diff / 3600) % 24;
+  input[2].value = Math.floor(diff / 60) % 60;
+  input[3].value = Math.floor(diff) % 60;
+}
+
+countDown();
+setInterval(countDown, 1000);
